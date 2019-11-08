c5fb63dfd1b56406c2d5fa748e56a9687a8a4056
Asiri Senith Manjitha <asirisenith@gmail.com>
2019-11-08 21:59:30 +0530

    center username & password

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
@@ -6,9 +6,10 @@ class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: new LoginPage(),
       theme: new ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.amber
       )
     );
   }
@@ -21,33 +22,71 @@ class LoginPageState extends State<LoginPage>{
   @override 
   Widget build(BuildContext context){
     return new Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.lightBlue,
       body: new Stack(
-       // fit: StackFit.expand,
+      fit: StackFit.expand,
         children: <Widget>[
           new Image(
             image: new AssetImage("assets/Projectlogo.png"),
-            fit: BoxFit.fill,
+            fit: BoxFit.contain,
             color: Colors.black26,
             colorBlendMode: BlendMode.darken,
-            width: 250,
-            height: 250,
+            //width: 250,
+            //height: 150,
+            
           ),
           new Column(
-            //mainAxisAlignment: mainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Form(
-                child: new Column(
-                  children: <Widget>[
-                    new TextFormField(
-                      decoration: new InputDecoration(
-                      hintText: "Enter UserName",
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ],
+                 child: new Theme(
+                      data: new ThemeData(
+                        brightness: Brightness.light,
+                        primarySwatch: Colors.red,
+                        inputDecorationTheme: new InputDecorationTheme(
+                          labelStyle: new TextStyle(
+                            color: Colors.red,
+                            fontSize: 27.0,
+                          )
+                        )
+                      ),
+                      child: new Container(
+                        padding: const EdgeInsets.all(55.0),
+                        
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                            hintText: "Enter UserName",
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                            hintText: "Enter Password",
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                     
+                    ],
               ),
-            )
+                      ),
+                 ),
+            ),
+            /*Column(
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 200.0,
+                      width: double.infinity,
+                    )
+                  ],
+                )
+              ],
+            )*/
           ]
           )
         ],
