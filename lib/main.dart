e3103a6ca39e5c6b6e8e8f224c3302f47c85d6a2
Asiri Senith Manjitha <asirisenith@gmail.com>
2019-11-13 22:52:54 +0530

    add LOGIN button and forgot password, and edit username,password

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class LoginPage extends StatefulWidget{
   @override  
   State createState() => new LoginPageState();
 }
+
 class LoginPageState extends State<LoginPage>{
   @override 
   Widget build(BuildContext context){
@@ -43,30 +44,79 @@ class LoginPageState extends State<LoginPage>{
                         //primarySwatch: Colors.red,
                         inputDecorationTheme: new InputDecorationTheme(
                           labelStyle: new TextStyle(
-                            //color: Colors.red,
+                            color: Colors.red,
                             fontSize: 77.0,
                           )
                         )
                       ),
+                      
                       child: new Container(
-                        padding: const EdgeInsets.all(55.0),
-                        
+                        padding: EdgeInsets.only(top:154.0,left: 45.0,right: 40.0),
                         child: new Column(
                           crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
                           new TextFormField(
                             decoration: new InputDecoration(
-                            hintText: "Enter UserName",
+                            labelText: "Enter UserName",
+                            labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white54
+                            )
                             ),
                           keyboardType: TextInputType.text,
                           ),
                           new TextFormField(
                             decoration: new InputDecoration(
-                            hintText: "Enter Password",
+                            labelText: "Enter Password",
+                            labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white54
+                            )
                             ),
                           keyboardType: TextInputType.text,
                           obscureText: true,
                         ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          alignment: Alignment(1.0, 0.0),
+                          padding: EdgeInsets.only(top: 15.0, left: 25.0),
+                          child: InkWell(
+                            child: Text('Forgot Password',
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                              fontSize: 13.0,
+                              decoration: TextDecoration.underline
+                            ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30.0),
+                        Container(
+                          height: 30.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            shadowColor: Colors.greenAccent,
+                            color: Colors.green,
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Center(
+                                child: Text(
+                                  'LOGIN',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
@@ -86,7 +136,7 @@ class LoginPageState extends State<LoginPage>{
                       height: 109.0,
                       width: 109.0,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35),
+                        //borderRadius: BorderRadius.circular(35),
                         /*border: Border.all(
                           color: Colors.white,
                           style: BorderStyle.solid,
@@ -100,9 +150,33 @@ class LoginPageState extends State<LoginPage>{
                   ],
                 )
              ],
-           )
-        ],
-      ),
+           ),
+           /*SizedBox(height: 15.0),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Text(
+                 'To Register Here',
+                 style: TextStyle(
+                   fontFamily: 'Montserrat'
+                ),
+               ),
+               SizedBox(width: 5.0),
+               InkWell(
+                 onTap: () {},
+                 child: Text('Register',
+                  style: TextStyle(
+                   color: Colors.green,
+                   fontFamily: 'Montserrat',
+                   fontWeight: FontWeight.bold,
+                   decoration: TextDecoration.underline
+                  ),
+                 ),
+               )
+             ],
+           )*/
+        ]
+      )
     );
   }
 }
